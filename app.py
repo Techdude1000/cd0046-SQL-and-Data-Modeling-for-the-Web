04cd28e55cc765954cdc506274a9bc83743916ba
Techdude1000 <Everett.lamborn@gmail.com>
2022-06-21 21:06:48 -0600

    FIxed requirementstxt

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 #----------------------------------------------------------------------------#
 # Imports
 #----------------------------------------------------------------------------#
-
+import collections
+import collections.abc
+collections.Callable = collections.abc.Callable
 import json
 import dateutil.parser
 import babel
